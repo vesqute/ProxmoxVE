037d5e1fb9462805d86a1e268613104d1aca3ad4
vesqute <vesqute@abwesend.de>
2025-03-20 10:37:14 +0100

    Update frigate.sh
    Signed-off-by: vesqute <vesqute@abwesend.de>

--- ct/frigate.sh
+++ ct/frigate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/remz1337/ProxmoxVE/remz/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/vesqute/ProxmoxVE/remz/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster) | Co-Author: remz1337
 # License: MIT | https://github.com/remz1337/ProxmoxVE/raw/remz/LICENSE
